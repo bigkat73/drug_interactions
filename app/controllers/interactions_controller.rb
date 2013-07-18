498e86d889c501020ee501750cf24be88b0cb01a
Fred Katona <fkatona@healthagen.net>
2013-07-18 15:57:19 -0600

    take in array as dumb comma delimited stuff

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -4,7 +4,7 @@ class InteractionsController < ApplicationController
   # GET /interactions
   # GET /interactions.json
   def index
-    med_ids = params[:medication_ids] || []
+    med_ids = params[:medication_ids].present? ? params[:medication_ids].split(',') : []
     @interaction = Interaction.new(med_ids)
     @interactions = @interaction.interactions
   end
