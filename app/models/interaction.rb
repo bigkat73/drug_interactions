7a99d57cb5efd9ffedc280c9eaf4e6f3711d2eec
Fred Katona <fkatona@healthagen.net>
2013-07-18 16:30:07 -0600

    ensure the api call doesnt blow up

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -37,16 +37,20 @@ class Interaction
   end
 
   def find_interaction_severity(medication_1, medication_2)
-    uri = URI.parse(BASE_API_URL)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri + "nui1=#{medication_1.nui}&nui2=#{medication_2.nui}&scope=2")
-    request["User-Agent"] = "My Ruby Script"
-    request["Accept"] = "application/json"
+    begin
+      uri = URI.parse(BASE_API_URL)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri + "nui1=#{medication_1.nui}&nui2=#{medication_2.nui}&scope=2")
+      request["User-Agent"] = "My Ruby Script"
+      request["Accept"] = "application/json"
 
-    output = JSON.parse(http.request(request).body)
-    if output["fullInteractionGroup"]["fullInteraction"].first["interactionCount"].to_i > 0
-      output["fullInteractionGroup"]["fullInteraction"].first["interactionTripleGroup"].first["interactionTriple"].first["severity"]
-    else
+      output = JSON.parse(http.request(request).body)
+      if output["fullInteractionGroup"]["fullInteraction"].first["interactionCount"].to_i > 0
+        output["fullInteractionGroup"]["fullInteraction"].first["interactionTripleGroup"].first["interactionTriple"].first["severity"]
+      else
+        'none'
+      end
+    rescue
       'none'
     end
   end
