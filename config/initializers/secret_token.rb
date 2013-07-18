8d4e0d9087a1f5e3d0a5b0b57ca8efc2fb6e352c
Fred Katona <fkatona@healthagen.net>
2013-07-18 10:06:14 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrugInteractions::Application.config.secret_key_base = '952773c88a0f38a30c1334fae3c6cb1e17fa8b36053c523a3b514889c6b276dc00c1d1a13df0d412d2c398ae323e0cc9dacd34bcb99d69119cf3913b8eb6f814'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrugInteractions::Application.config.session_store :cookie_store, key: '_drug_interactions_session'
