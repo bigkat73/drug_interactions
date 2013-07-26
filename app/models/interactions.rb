bbb524e4816bc3c2ff639672216b66cf8e3b3bd1
Fred Katona <fkatona@healthagen.net>
2013-07-26 11:04:42 -0600

    return from api cleaned up

--- app/models/interactions.rb
+++ app/models/interactions.rb
@@ -12,7 +12,7 @@ class Interactions
       (i+1).upto(@medication_ids.size-1) do |j|
       	medication1 = Medication.where(id: m_id).first
       	medication2 = Medication.where(id: @medication_ids[j]).first
-        severity, formatted_interaction = medication1.interaction_severity_to(medication2)
+        severity = medication1.interaction_severity_to(medication2)
         if severity != 'none'
           interactions << medication1.formatted_interaction
         end
