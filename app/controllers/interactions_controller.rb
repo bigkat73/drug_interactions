bb6bf5974571650e71325663461a12cfdaf090ad
fdk <fdkatona@gmail.com>
2013-08-12 12:28:04 -0600

    start to add angular

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -4,9 +4,6 @@ class InteractionsController < ApplicationController
   # GET /interactions
   # GET /interactions.json
   def index
-    med_ids = params[:medication_ids].present? ? params[:medication_ids].split(',') : []
-    @interaction = Interactions.new(med_ids)
-    @interactions = @interaction.interactions
   end
 
 end
